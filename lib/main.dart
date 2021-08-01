0669e3da1fc357372b6dca1cfc246bb6a151d527
Christoph Hartmann <c.hartmann@carepayinternational.com>
2021-08-01 15:36:51 +0200

    loading firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,76 @@ import 'MatchCard.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-void main(){
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized(); // From firebase init docs
+  runApp(App());
+}
+
+// From firebase init docs
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Loading();
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Loading();
+      },
+    );
+  }
+}
+
+class Loading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Letss - Loading",
+      home: SafeArea(
+        child: Scaffold(
+          body: Column(
+            children: <Widget>[
+              Center(
+                heightFactor: 10,
+                // Show logo instead moving up and down
+                child: Text('Loading'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SomethingWentWrong extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      'Something Went Wrong',
+      textDirection: TextDirection.ltr,
+    );
+  }
 }
 
 /// This is the main application widget.
