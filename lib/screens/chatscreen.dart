29e2b9377bd7e42d00faeae04b966a165e69e549
Christoph Hartmann <c.hartmann@carepayinternational.com>
2021-09-01 23:29:36 +0200

    added chat button and improved chat styling

--- lib/screens/chatscreen.dart
+++ lib/screens/chatscreen.dart
@@ -4,13 +4,37 @@ import '../widgets/messagebubble.dart';
 import '../models/chat.dart';
 import '../models/message.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
   const ChatScreen({
-    Key? key,
     required this.chat,
+    Key? key,
   }) : super(key: key);
 
   final Chat chat;
+
+  @override
+  ChatScreenState createState() {
+    return ChatScreenState();
+  }
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final textController = TextEditingController();
+
+  @override
+  void dispose() {
+    textController.dispose();
+    super.dispose();
+  }
+
+  String? validateMessage(String? value) {
+    String val = value == null ? "" : value;
+    if (val == "")
+      return 'Please enter a valid message';
+    else
+      return null;
+  }
 
   List<Widget> _createMessages(List<Message> messages) {
     List<Widget> messageWidgets = [];
@@ -18,7 +42,8 @@ class ChatScreen extends StatelessWidget {
     messageWidgets.add(const SizedBox(height: 10));
     for (int i = 0; i < messages.length; i++) {
       messageWidgets.add(MessageBubble(
-          message: chat.messages[i].message, me: chat.messages[i].me));
+          message: widget.chat.messages[i].message,
+          me: widget.chat.messages[i].me));
       messageWidgets.add(const SizedBox(height: 10));
     }
     messageWidgets.add(const SizedBox(height: 10));
@@ -27,27 +52,58 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // TODO use ChatConsumer here and access chat through index similar to activity
     return Scaffold(
       body: SafeArea(
           child: HeaderScreen(
-        header: Row(children: [
-          SizedBox(width: 50, height: 50, child: chat.person.profilePic),
-          const SizedBox(width: 10),
-          Text(this.chat.person.name,
-              style: Theme.of(context).textTheme.headline1)
-        ]),
-        child: Column(children: [
-          Expanded(
-              child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: ListView(
-                      shrinkWrap: true,
-                      children: _createMessages(this.chat.messages)))),
-          TextFormField(
-            decoration: InputDecoration(
-                border: UnderlineInputBorder(), labelText: 'Say something...'),
-          )
-        ]),
+        header: ListTile(
+            leading: widget.chat.person.thumbnail,
+            title: Text(widget.chat.person.name,
+                style: Theme.of(context).textTheme.headline1)),
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Flexible(
+                child: ListView(
+                    shrinkWrap: true,
+                    children: _createMessages(widget.chat.messages)),
+              ),
+              Form(
+                  key: _formKey,
+                  child: (Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                            child: TextFormField(
+                          controller: textController,
+                          validator: validateMessage,
+                          decoration: InputDecoration(
+                              border: UnderlineInputBorder(),
+                              labelText: 'Say something...'),
+                        )),
+                        const SizedBox(width: 5),
+                        RawMaterialButton(
+                            onPressed: () {
+                              if (_formKey.currentState!.validate()) {
+                                String message = textController.text;
+                                print(message);
+                              }
+                            },
+                            elevation: 2.0,
+                            fillColor: Colors.white,
+                            child: Icon(Icons.send_rounded,
+                                size: 20.0, color: Colors.orange),
+                            shape: CircleBorder(),
+                            padding: EdgeInsets.all(10),
+                            materialTapTargetSize:
+                                MaterialTapTargetSize.shrinkWrap,
+                            constraints: BoxConstraints())
+                      ]))),
+            ]),
         back: true,
       )),
     );
