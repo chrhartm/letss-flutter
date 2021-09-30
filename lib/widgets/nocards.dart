e36e8fb2ef3afa0f5f3773c0f58e644c2bc679bc
Christoph Hartmann <c.hartmann@carepayinternational.com>
2021-09-30 21:39:23 +0200

    forgot to add nocards

---
+++ lib/widgets/nocards.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class NoCards extends StatelessWidget {
+  const NoCards({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      children: [
+        const SizedBox(height: 100),
+        Text("🏃💨", style: Theme.of(context).textTheme.headline1),
+        Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              "You browsed all available activities",
+              style: Theme.of(context).textTheme.headline2,
+              textAlign: TextAlign.center,
+            )),
+        const SizedBox(height: 100),
+        Text("🧐", style: Theme.of(context).textTheme.headline1),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 50),
+          child: Text("Why not add some yourself?",
+              style: Theme.of(context).textTheme.headline2,
+              textAlign: TextAlign.center),
+        )
+      ],
+    ));
+  }
+}
