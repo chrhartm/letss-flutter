f7883e94a1bd4e593c140fb785daa640c70598ee
Christoph Hartmann <christophartmann@gmail.com>
2022-03-06 19:00:25 +0100

    added status and createdAt to flag

--- lib/models/flag.dart
+++ lib/models/flag.dart
@@ -6,18 +6,23 @@ class Flag {
   Person flagged;
   Activity? activity;
   String message;
+  String status;
+  DateTime createdAt;
 
   Flag(
       {required this.flagger,
       required this.flagged,
       required this.message,
-      activity});
+      activity}): status = "NEW",
+        createdAt = DateTime.now();
 
   Map<String, dynamic> toJson() {
     Map<String, dynamic> payload = {
       "flagger": flagger.uid,
       "flagged": flagged.uid,
-      "message": message
+      "message": message,
+      "status": status,
+      "createdAt": createdAt,
     };
     if (activity != null) {
       payload["activity"] = activity!.uid;
