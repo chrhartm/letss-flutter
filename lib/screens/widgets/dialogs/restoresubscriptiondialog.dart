7c3c0a88b9fc86b61837121c6031d47e1694e7c4
Christoph Hartmann <christophartmann@gmail.com>
2022-03-20 16:50:50 +0100

    Dialog for subscription renewal

---
+++ lib/screens/widgets/dialogs/restoresubscriptiondialog.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:letss_app/backend/analyticsservice.dart';
+import 'package:letss_app/screens/widgets/dialogs/mydialog.dart';
+
+import '../../../backend/StoreService.dart';
+import '../../../backend/loggerservice.dart';
+
+class RestoreSubscriptionDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MyDialog(
+      title: 'Badge renewal 📅',
+      content: MyDialog.TextContent(
+          'Thanks again for supporting us! We need to quickly check your subscription status to keep it active.'),
+      action: () {
+        analytics.logEvent(name: "Support_Restore");
+        try {
+          StoreService().restorePurchases();
+        } catch (e) {
+          LoggerService.log("Error in restoring purchase. " + e.toString(),
+              level: "w");
+        }
+        Navigator.of(context, rootNavigator: true).pop('dialog');
+      },
+      actionLabel: 'Check',
+    );
+  }
+}
