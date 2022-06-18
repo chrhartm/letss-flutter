1ecbd215db796800d27eedbc3cdf93b2431ba620
Christoph Hartmann <christophartmann@gmail.com>
2022-06-18 21:46:14 +0200

    added loader to travelling

--- lib/screens/signup/travel.dart
+++ lib/screens/signup/travel.dart
@@ -5,6 +5,8 @@ import 'package:letss_app/models/searchlocation.dart';
 import 'package:letss_app/provider/activitiesprovider.dart';
 import 'package:letss_app/provider/userprovider.dart';
 import 'package:letss_app/screens/signup/widgets/traveldisabled.dart';
+import 'package:letss_app/screens/widgets/other/loader.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_typeahead/flutter_typeahead.dart';
 
@@ -20,59 +22,71 @@ class Travel extends StatelessWidget {
                   back: true,
                   header: "Travel",
                   child: SingleChildScrollView(
-                      child: user.searchEnabled
-                          ? Column(children: [
-                              TypeAheadField(
-                                hideOnError: true,
-                                hideOnEmpty: false,
-                                textFieldConfiguration: TextFieldConfiguration(
-                                  autofocus: false,
-                                  decoration: InputDecoration(
-                                    isDense: true,
-                                    border: OutlineInputBorder(),
-                                    label: Text('Search location'),
-                                    floatingLabelBehavior:
-                                        FloatingLabelBehavior.never,
+                      child: LoaderOverlay(
+                          useDefaultLoading: false,
+                          overlayWidget: Center(
+                            child: Loader(),
+                          ),
+                          overlayOpacity: 0.6,
+                          child: user.searchEnabled
+                              ? Column(children: [
+                                  TypeAheadField(
+                                    hideOnError: true,
+                                    hideOnEmpty: false,
+                                    textFieldConfiguration:
+                                        TextFieldConfiguration(
+                                      autofocus: false,
+                                      decoration: InputDecoration(
+                                        isDense: true,
+                                        border: OutlineInputBorder(),
+                                        label: Text('Search location'),
+                                        floatingLabelBehavior:
+                                            FloatingLabelBehavior.never,
+                                      ),
+                                    ),
+                                    suggestionsCallback: (pattern) async {
+                                      return await LocationService.getLocations(
+                                          pattern);
+                                    },
+                                    itemBuilder:
+                                        (context, SearchLocation location) {
+                                      return ListTile(
+                                          title: Text(location.description));
+                                    },
+                                    noItemsFoundBuilder: (context) => Container(
+                                        child: Padding(
+                                            padding: EdgeInsets.all(8),
+                                            child: Text(
+                                                "No matching places found"))),
+                                    onSuggestionSelected:
+                                        (SearchLocation location) async {
+                                      LatLongLocation? loc =
+                                          await LocationService.getLatLong(
+                                              location);
+                                      if (loc != null) {
+                                        context.loaderOverlay.show();
+
+                                        user
+                                            .updatePerson(
+                                                latitude: loc.latitude,
+                                                longitude: loc.longitude,
+                                                context: context)
+                                            .then((_) =>
+                                                Provider.of<ActivitiesProvider>(
+                                                        context,
+                                                        listen: false)
+                                                    .resetAfterLocationChange())
+                                            .then((_) {
+                                          context.loaderOverlay.hide();
+                                          Navigator.pop(context);
+                                        });
+                                      } else {
+                                        Navigator.pop(context);
+                                      }
+                                    },
                                   ),
-                                ),
-                                suggestionsCallback: (pattern) async {
-                                  return await LocationService.getLocations(
-                                      pattern);
-                                },
-                                itemBuilder:
-                                    (context, SearchLocation location) {
-                                  return ListTile(
-                                      title: Text(location.description));
-                                },
-                                noItemsFoundBuilder: (context) => Container(
-                                    child: Padding(
-                                        padding: EdgeInsets.all(8),
-                                        child:
-                                            Text("No matching places found"))),
-                                onSuggestionSelected:
-                                    (SearchLocation location) async {
-                                  LatLongLocation? loc =
-                                      await LocationService.getLatLong(
-                                          location);
-                                  if (loc != null) {
-                                    user
-                                        .updatePerson(
-                                            latitude: loc.latitude,
-                                            longitude: loc.longitude,
-                                            context: context)
-                                        .then((_) =>
-                                            Provider.of<ActivitiesProvider>(
-                                                    context,
-                                                    listen: false)
-                                                .resetAfterLocationChange())
-                                        .then((_) => Navigator.pop(context));
-                                  } else {
-                                    Navigator.pop(context);
-                                  }
-                                },
-                              ),
-                            ])
-                          : TravelDisabled()))));
+                                ])
+                              : TravelDisabled())))));
     });
   }
 }
