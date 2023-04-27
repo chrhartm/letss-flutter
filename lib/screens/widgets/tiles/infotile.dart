052ea07a5425d46d6f489814fcb52e11304b4e55
Christoph Hartmann <christophartmann@gmail.com>
2023-04-27 16:47:21 +0200

    added infotile

---
+++ lib/screens/widgets/tiles/infotile.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'tile.dart';
+
+class InfoTile extends StatelessWidget {
+  const InfoTile({Key? key, required this.title, required this.text})
+      : super(key: key);
+
+  final String title;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Tile(
+        child: Column(children: [
+      Align(
+          alignment: Alignment.topLeft,
+          child: Text(title, style: Theme.of(context).textTheme.titleLarge)),
+      const SizedBox(height: 5),
+      Align(
+        alignment: Alignment.topLeft,
+        child: Text(text,
+            strutStyle: StrutStyle(forceStrutHeight: true),
+            style: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(color: Theme.of(context).colorScheme.secondary)),
+      )
+    ]));
+  }
+}
