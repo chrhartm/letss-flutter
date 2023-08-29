2f12f7d52f49d96ddff50b117108ec2cc9e9da36
Christoph Hartmann <christophartmann@gmail.com>
2023-08-29 14:11:06 +0200

    added link reading ability to description

--- lib/screens/widgets/tiles/texttile.dart
+++ lib/screens/widgets/tiles/texttile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'tile.dart';
+import 'package:flutter_linkify/flutter_linkify.dart';
 
 class TextTile extends StatelessWidget {
   const TextTile({Key? key, required this.title, required this.text})
@@ -17,11 +19,21 @@ class TextTile extends StatelessWidget {
           child: Text(title, style: Theme.of(context).textTheme.titleLarge)),
       const SizedBox(height: 5),
       Align(
-        alignment: Alignment.topLeft,
-        child: Text(text,
+          alignment: Alignment.topLeft,
+          child: Linkify(
+            onOpen: (link) async {
+              if (!await launchUrl(Uri.parse(link.url))) {
+                throw Exception('Could not launch ${link.url}');
+              }
+            },
+            text: text,
+            style: Theme.of(context).textTheme.bodyLarge,
             strutStyle: StrutStyle(forceStrutHeight: true),
-            style: Theme.of(context).textTheme.bodyLarge),
-      )
+            linkStyle: Theme.of(context)
+                .textTheme
+                .bodyLarge!
+                .copyWith(decoration: TextDecoration.underline),
+          ))
     ]));
   }
 }
