54258431e6dfb71e9e6a98adf30bfba804162d13
Christoph Hartmann <christophartmann@gmail.com>
2023-11-07 22:01:36 +0100

    fixed signup explainer

--- lib/screens/signup/signupexplainer.dart
+++ lib/screens/signup/signupexplainer.dart
@@ -15,39 +15,32 @@ class SignUpExplainer extends StatelessWidget {
   Widget build(BuildContext context) {
     List<Widget> items = [
       ListTile(
-          leading: ButtonAction(icon: Icons.add, heroTag: "like"),
-          title: Text(AppLocalizations.of(context)!.signupExplainerLikeTitle),
-          subtitle:
-              Text(AppLocalizations.of(context)!.signupExplainerLikeSubtitle)),
-      ListTile(
-          leading: ButtonAction(
-            icon: Icons.horizontal_rule,
-          ),
-          title: Text(AppLocalizations.of(context)!.signupExplainerNoTitle),
-          subtitle:
-              Text(AppLocalizations.of(context)!.signupExplainerNoSubtitle)),
-      ListTile(
           leading: ButtonAction(icon: Icons.add, heroTag: "add"),
           title: Text(AppLocalizations.of(context)!.signupExplainerAddTitle),
           subtitle:
               Text(AppLocalizations.of(context)!.signupExplainerAddSubtitle)),
+      ListTile(
+          leading: ButtonAction(icon: Icons.lightbulb),
+          title: Text(AppLocalizations.of(context)!.signupExplainerIdeaTitle),
+          subtitle:
+              Text(AppLocalizations.of(context)!.signupExplainerIdeaSubtitle)),
       ListTile(
           leading: ButtonAction(icon: Icons.edit, heroTag: "edit"),
           title: Text(AppLocalizations.of(context)!.signupExplainerEditTitle),
           subtitle:
               Text(AppLocalizations.of(context)!.signupExplainerEditSubtitle)),
       ListTile(
-          leading: ButtonAction(
-              icon: Platform.isIOS ? Icons.ios_share : Icons.share),
-          title: Text(AppLocalizations.of(context)!.signupExplainerShareTitle),
-          subtitle:
-              Text(AppLocalizations.of(context)!.signupExplainerShareSubtitle)),
-      ListTile(
           leading: ButtonAction(icon: Icons.archive),
           title:
               Text(AppLocalizations.of(context)!.signupExplainerArchiveTitle),
           subtitle: Text(
               AppLocalizations.of(context)!.signupExplainerArchiveSubtitle)),
+      ListTile(
+          leading: ButtonAction(
+              icon: Platform.isIOS ? Icons.ios_share : Icons.share),
+          title: Text(AppLocalizations.of(context)!.signupExplainerShareTitle),
+          subtitle:
+              Text(AppLocalizations.of(context)!.signupExplainerShareSubtitle)),
       ListTile(
           leading: ButtonAction(icon: Icons.settings),
           title:
